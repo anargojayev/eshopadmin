f9941330427da2011308469ceafe9899131f31bd
Anar Gojayev <anarqoff@gmail.com>
2021-11-18 21:02:10 +0400

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eshopadmin.Models;
+using System.IO;
+
+namespace eshopadmin.Controllers
+{
+    public class AdminController : Controller
+    {
+        DataClassDataContext DataBase = new DataClassDataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Category()
+        {
+            List<Category> kateqoriya = new List<Category>();
+            kateqoriya = DataBase.Categories.ToList();
+            return View(kateqoriya);
+        }
+        [HttpPost]
+        public ActionResult Category(Category kateqoriya)
+        {
+            if(kateqoriya.CategoryID == 0)
+            {
+                DataBase.Categories.InsertOnSubmit(kateqoriya);
+            }
+            else
+            {
+                Category selectedCategory = DataBase.Categories.SingleOrDefault(x => x.CategoryID == kateqoriya.CategoryID);
+                selectedCategory.CategoryName = kateqoriya.CategoryName;
+            }
+            DataBase.SubmitChanges();
+            return RedirectToAction("Category");
+        }
+        public ActionResult DeleteCategoryRow(int id)
+        {
+            DataBase.Categories.DeleteOnSubmit(DataBase.Categories.SingleOrDefault(x => x.CategoryID == id));
+            DataBase.SubmitChanges();
+            return RedirectToAction("Category");
+        }
+        [HttpGet]
+        public ActionResult Product()
+        {
+            List<ProductView> mehsul = new List<ProductView>();
+            mehsul = DataBase.ProductViews.ToList();
+            List<Category> kateqoriya = new List<Category>();
+            kateqoriya = DataBase.Categories.ToList();
+            ViewBag.Category = kateqoriya.ToList();
+            return View(mehsul);
+        }
+        [HttpPost]
+        public ActionResult Product(Product mehsul)
+        {
+            if (mehsul.ProductID == 0)
+            {
+                DataBase.Products.InsertOnSubmit(mehsul);
+            }
+            else
+            {
+                ProductView selectedProduct = DataBase.ProductViews.SingleOrDefault(x => x.ProductID == mehsul.ProductID);
+                selectedProduct.ProductName = mehsul.ProductName;
+                selectedProduct.ProductPrice = mehsul.ProductPrice;
+                selectedProduct.ProductDiscount = mehsul.ProductDiscount;
+                selectedProduct.ProductInfo = mehsul.ProductInfo;
+            }
+            DataBase.SubmitChanges();
+            return RedirectToAction("Product");
+        }
+        public ActionResult DeleteProductRow(int id)
+        {
+            DataBase.Products.DeleteOnSubmit(DataBase.Products.SingleOrDefault(x => x.ProductID == id));
+            DataBase.SubmitChanges();
+            return RedirectToAction("Product");
+        }
+    }
+}
---
+++ assets/js/editmodal.js
@@ -0,0 +1,31 @@
+﻿    $(function () {
+        $('#table .fa-edit').click(function () {
+            $(this).parents('tr').find('td').each(function (index) {
+                $(`[name="${$('thead tr th').eq(index).attr('forinput')}"]`).val($(this).text())
+            })
+        })
+    })
+$(function () {
+
+    $('#AddCategory').click(function () {
+        $('#customFile').attr('required', 'required')
+        $('#categoryname').val(null)
+        $('#categoryid').val(0)
+    })
+    $('#AddProduct').click(function () {
+        $('#customFile').attr('required', 'required')
+        $('#productname').val(null)
+        $('#productid').val(0)
+    })
+    //$('#table .fa-edit').click(function () {
+    //    $('#categoryname').val($(this).parents('tr').find('td').eq(2).text())
+    //    $('#categoryid').val($(this).parents('tr').find('td').eq(0).text())
+    //})
+    //$('#table .fa-edit').click(function () {
+    //    $('#productname').val($(this).parents('tr').find('td').eq(2).text())
+    //    $('#productid').val($(this).parents('tr').find('td').eq(0).text())
+    //    $('#productprice').val($(this).parents('tr').find('td').eq(3).text())
+    //    $('#productdiscount').val($(this).parents('tr').find('td').eq(4).text())
+    //    $('#productinfo').val($(this).parents('tr').find('td').eq(5).text())
+    //})
+})
